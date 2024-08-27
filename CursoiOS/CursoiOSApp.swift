bb1c19240743a036e1afd3257d9602e2206dfedf
Iñigo Esteban <nack_z4@hotmail.com>
2024-08-27 18:32:37 +0200

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Iñigo Esteban Gómez on 27/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
